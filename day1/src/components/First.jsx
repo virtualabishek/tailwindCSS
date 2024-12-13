ea409d6f9cfb378ffa4249414ed2e33f76c2d515
virtualabishek <717abishekneupane@gmail.com>
2024-12-13 06:47:29 +0545

    Done For Now. Just Basic Setup

--- day1/src/components/First.jsx
+++ day1/src/components/First.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 const First = () => {
   return (
-    <>
-      <h1 className=" px-10 text-3xl text-center text-yellow-700">
+    <div className=" min-h-screen grid place-content-center">
+      <h1 className="text-3xl text-center text-yellow-700">
         Hi. This is me, just being me.
       </h1>
-    </>
+      <br />
+      <br />
+      <div className="grid place-content-center bg-emerald-500 w-52 h-52 rounded-full shadow-2xl">
+        <div className="bg-white-200 w-32 h-32 rounded-full"></div>
+      </div>
+    </div>
   );
 };
 
